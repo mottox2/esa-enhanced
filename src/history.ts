62a56aeb2b6de5a4cfafaef4f8c56ea5061e994c
mottox2 <y.mottox2@gmail.com>
2020-05-19 01:57:44 +0900

    Save post history

--- src/history.ts
+++ src/history.ts
@@ -3,37 +3,84 @@ const selectors = {
   name: ".post-title__name",
 };
 
+type Post = {
+  id: number;
+  name: string;
+  category: string;
+  tags: string[];
+  wip: boolean;
+  star: boolean;
+  created_by: {
+    name: string;
+    icon: string;
+  };
+};
+
+type HistoryStore = {
+  posts: Record<number, Post>;
+  ids: number[];
+};
+
 const exist = (selector: string) => Boolean(document.querySelector(selector));
 const selectAll = (selector: string) =>
   Array.from(document.querySelectorAll(selector));
 const select = (selector: string) => document.querySelector(selector);
 
+const historyStorage = {
+  get: (cb: (store: HistoryStore) => void) => {
+    chrome.storage.sync.get(["posts", "ids"], (result: HistoryStore) => {
+      cb({ posts: {}, ids: [], ...result });
+    });
+  },
+  set: (store: HistoryStore, cb: () => void) => {
+    chrome.storage.sync.set(store, () => {
+      cb();
+    });
+  },
+};
+
+const pushHistory = (post: Post) => {
+  historyStorage.get((result) => {
+    const newStore: HistoryStore = {
+      posts: {
+        [post.id]: post,
+        ...result.posts,
+      },
+      ids: [post.id, ...result.ids.filter((id) => id !== post.id)], // new Set
+    };
+
+    // 件数より多かったら削除する何かの処理
+    console.log(result);
+
+    historyStorage.set(newStore, () => {
+      console.log("saved:", newStore);
+    });
+  });
+};
+
 const init = () => {
   if (document.URL.search(/esa.io\/posts\/\d+/) > -1) {
-    let results = {};
-    results["id"] = Number(select(selectors.id).textContent.replace("#", ""));
-    results["name"] = select(selectors.name).textContent;
-
-    results["category"] = selectAll(".category-path__link")
-      .map((node) => node.textContent)
-      .join("/");
-    results["tags"] = selectAll(".post-title__tag").map((node) =>
-      node.textContent.trim()
-    );
-
-    results["wip"] = exist(".is-wip.post-title");
-    results["star"] = exist(".is-starred.js-star-button");
-
     const author = select(".post-author");
-    results["created_by"] = {};
-    results["created_by"]["name"] = author.querySelector(
-      ".post-author__user > a"
-    ).textContent;
-    results["created_by"]["icon"] = (author.querySelector(
-      ".thumbnail__image"
-    ) as HTMLImageElement).src;
+    let post: Post = {
+      id: Number(select(selectors.id).textContent.replace("#", "")),
+      name: select(selectors.name).textContent,
+      category: selectAll(".category-path__link")
+        .map((node) => node.textContent)
+        .join("/"),
+      tags: selectAll(".post-title__tag").map((node) =>
+        node.textContent.trim()
+      ),
+      wip: exist(".is-wip.post-title"),
+      star: exist(".is-starred.js-star-button"),
+      created_by: {
+        name: author.querySelector(".post-author__user > a").textContent,
+        icon: (author.querySelector(".thumbnail__image") as HTMLImageElement)
+          .src,
+      },
+    };
+    console.log(post);
 
-    console.log(results);
+    pushHistory(post);
   }
 };
 
